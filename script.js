574d2d34f04edc815f5bee62971a089d29d4b59b
Matteo <matteo.zuccolo.25@stud.itsaltoadriatico.it>
2026-03-29 12:35:33 +0200

    feat: add to basket

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// carrello del cliente
+const basket = [];
+
+// rappresenta la lista di prodotti nell'HTML
 const productList = document.getElementById("product-list");
 
 startApp();
@@ -8,12 +12,6 @@ startApp();
 async function startApp() {
     const db = await loadDatabase();
     for (let articolo of db.listino) {
-        console.log(articolo.id);
-        console.log(articolo.titolo);
-        console.log(articolo.immagine);
-        console.log(articolo.descrizione);
-        console.log(articolo.quantita);
-        console.log(articolo.prezzo);
         const item = document.createElement("div");
         item.innerHTML = `
             <img src="${articolo.immagine}" width="200" height="100">
@@ -22,11 +20,43 @@ async function startApp() {
             <p>Quantità: ${articolo.quantita}</p>
             <p>Prezzo: ${articolo.prezzo} €</p>
             <input type="number" id="customerQuantity-${articolo.id}" min="0" max="${articolo.quantita}">
-            <button id="basket-${articolo.id}">Aggiugi al carrello</button>
+            <button id="addToBasket-${articolo.id}">Aggiugi al carrello</button>
             <hr />
         `;
         productList.append(item);
     }
+
+    const buttons = document.querySelectorAll('main button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', function (event) {
+            // id del bottone
+            const fullId = event.target.getAttribute("id");
+            // id del prodotto interessato
+            const id = fullId.split("-")[1];
+            // quantità ordinata dal cliente
+            const customerQuantity = document.getElementById(`customerQuantity-${id}`).value;
+            const item = {
+                id: Number(id),
+                quantity: Number(customerQuantity)
+            };
+
+            // cerco il prodotto nella lista
+            let found = false;
+            for (let it of basket) {
+                if (it.id === item.id) {
+                    // elemento già presente: aggiorno la quantità
+                    found = true;
+                    it.quantity += item.quantity;
+                    break;
+                }
+            }
+            if (!found) {
+                // elemento non presente: lo inserisco nel carrello
+                basket.push(item);
+            }
+        });
+    });
+
 }
 
 // legge i dati dal database json
@@ -35,6 +65,6 @@ async function loadDatabase(db) {
         let response = await fetch("db.json");
         return await response.json();
     } catch (error) {
-        console.log(error);
+        alert(error);
     }
 }
